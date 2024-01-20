6ada6d8f89262c8ef75c2d8785142ce8bcfa4824
saurabhraj123 <me4saurabh4u@gmail.com>
2024-01-20 23:33:49 +0530

    fix form styling

--- Travel-list/src/components/FormSection.jsx
+++ Travel-list/src/components/FormSection.jsx
@@ -23,26 +23,24 @@ const FormSection = ({ handleAddItem }) => {
   return (
     <div className={classes.form}>
       <p className={classes.title}>What do you need for your 😍 trip?</p>
-      <select
-        className={classes.itemCountContainer}
-        value={quantity}
-        onChange={(e) => setQuantity(e.target.value)}
-      >
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option key={num} value={num}>
-            {num}
-          </option>
-        ))}
-      </select>
+      <div className={classes.itemCountContainer}>
+        <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+          {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+            <option key={num} value={num}>
+              {num}
+            </option>
+          ))}
+        </select>
+      </div>
       <input
         type="text"
         value={itemInput}
-        placeholder="Item..."
+        placeholder="Enter an item..."
         className={classes.itemInput}
         onChange={(e) => setItemInput(e.target.value)}
       />
       <button className={classes.addButton} onClick={addItem}>
-        Add
+        ADD
       </button>
     </div>
   );
--- Travel-list/src/components/Header.jsx
+++ Travel-list/src/components/Header.jsx
@@ -1,7 +1,13 @@
 import classes from "./Header.module.css";
 
 const Header = () => {
-  return <h1 className={classes.header}>🌴 FAR AWAY 💼</h1>;
+  return (
+    <h1 className={classes.container}>
+      <div>🌴</div>
+      <div>FAR AWAY</div>
+      <div className={classes.bagName}>💼</div>
+    </h1>
+  );
 };
 
 export default Header;
